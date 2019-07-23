2c11be598430079e56640ffbd285896e194fa85a
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-07-22 23:40:48 -0300

    finishing crud challenge

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const server = express();
 server.use(express.json());
 
 const projects = [
-  { id: "0", title: "projeto A" },
-  { id: "1", title: "projeto A" }
+  { id: "0", title: "projeto A", tasks: [] },
+  { id: "1", title: "projeto A", tasks: [] }
 ];
 
 server.get("/projects", (req, res) => {
@@ -22,12 +22,32 @@ server.post("/projects", (req, res) => {
   return res.json(projects);
 });
 
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(project => project.id === id);
+  project.tasks.push(title);
+
+  return res.json(projects);
+});
+
 server.put("/projects/:id", (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
-  projects[id] = { id: projects[id].id, title };
+  const project = projects.find(project => project.id === id);
 
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+  projects.splice(projectIndex, 1);
   return res.json(projects);
 });
 
